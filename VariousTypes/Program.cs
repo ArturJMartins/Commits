0701f45532bf8b902a4968806384191b06b5d982
Artur Martins <arturjcmartins.dev@gmail.com>
2024-02-23 11:26:12 +0000

    add variaveis inteiras

---
+++ VariousTypes/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace VariousTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            short a = 1;
+            int b = 2;
+            long c = 3L;
+            ushort d = 4;
+            uint e = 5U;
+            ulong f = 6UL;
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+            Console.WriteLine(d);
+            Console.WriteLine(e);
+            Console.WriteLine(f);
+        }
+    }
+}
