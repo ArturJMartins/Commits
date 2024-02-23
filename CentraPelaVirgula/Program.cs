e2523c715571db1829c6e3309f2374bf6e234d44
Artur Martins <arturjcmartins.dev@gmail.com>
2024-02-23 17:23:17 +0000

    Tentativa de 4 inputs

---
+++ CentraPelaVirgula/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+using System.Threading;
+
+namespace CentraPelaVirgula
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CultureInfo.DefaultThreadCurrentCulture = CultureInfo.
+            InvariantCulture;
+            
+
+            
+                Console.WriteLine("Insira 4 números reais: ");
+                string numeros = Console.ReadLine();
+                string numeros2 = Console.ReadLine();
+                string numeros3 = Console.ReadLine();
+                string numeros4 = Console.ReadLine();
+
+                Console.WriteLine(numeros);
+                Console.WriteLine(numeros);
+                Console.WriteLine(numeros);
+                Console.WriteLine(numeros);
+
+
+            
+
+        }
+    }
+}
