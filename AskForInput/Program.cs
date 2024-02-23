de84026445a38bc9b21e5c0245803cf9d8e6c00f
Artur Martins <arturjcmartins.dev@gmail.com>
2024-02-23 14:56:27 +0000

    Inserir numeros, guardar em variaveis e imprimir a soma

---
+++ AskForInput/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace AskForInput
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world!");
+            Console.WriteLine("Insira um número inteiro: ");
+            string usuario = Console.ReadLine();
+            int escolha = int.Parse(usuario);
+
+            Console.WriteLine("Insira um número real: ");
+            string usuario2 = Console.ReadLine();
+            float escolha2 = float.Parse(usuario2);
+
+            Console.WriteLine("A soma dos dois números é " +
+             $"{escolha + escolha2}.");
+
+
+            
+        }
+    }
+}
