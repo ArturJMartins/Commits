43be31d196de49bd7baccca557c1ed966b3e9493
Artur Martins <arturjcmartins.dev@gmail.com>
2024-02-23 13:56:12 +0000

    Variaveis do tipo string

---
+++ ManyStrings/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ManyStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //variaveis do tipo string
+            string frase1 = "O (\\n) \né um caracter de escape e este " +
+            "unicode \\u2605 \u2605 é uma estrela.";
+
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine(frase1);
+        }
+    }
+}
