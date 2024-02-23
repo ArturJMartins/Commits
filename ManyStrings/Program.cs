3d485a1adb1d5ca5d980ae0ec59a55db240b470d
Artur Martins <arturjcmartins.dev@gmail.com>
2024-02-23 14:40:00 +0000

    Casas decimais, percentagem, hexadecimal e moeda

--- ManyStrings/Program.cs
+++ ManyStrings/Program.cs
@@ -6,13 +6,16 @@ namespace ManyStrings
     {
         static void Main(string[] args)
         {
+            double xx = 1.23456;
+            int ii = 19;
+
             //variaveis do tipo string
             string frase1 = "O (\\n) \né um caracter de escape e este " +
             "unicode \\u2605 \u2605 é uma estrela.";
 
             //variaveis string com concatenação e por interpolação
             int idade = 28;
-            int velhice = 40;
+            int velhice = 50;
             string nome = "Artur " + idade;
             string concatenar ="Ola " + nome + ".";
 
@@ -25,6 +28,12 @@ namespace ManyStrings
             Console.WriteLine(concatenar);
             Console.WriteLine(interpolar);
             Console.WriteLine(interpolar2);
+            
+            Console.WriteLine($"xx em número real com 2 casas = {xx:f2}");
+            Console.WriteLine($"xx em percentagem com 1 casa = {xx:p1}");
+
+            Console.WriteLine($"ii = {ii:X}");
+            Console.WriteLine($"ii em moeda = {ii:C}");
         }
     }
 }
