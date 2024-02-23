f21af9f97d8450b2fba465288a38afd245872728
Artur Martins <arturjcmartins.dev@gmail.com>
2024-02-23 14:14:19 +0000

    Concatenação e interpolação de strings

--- ManyStrings/Program.cs
+++ ManyStrings/Program.cs
@@ -10,8 +10,21 @@ namespace ManyStrings
             string frase1 = "O (\\n) \né um caracter de escape e este " +
             "unicode \\u2605 \u2605 é uma estrela.";
 
+            //variaveis string com concatenação e por interpolação
+            int idade = 28;
+            int velhice = 40;
+            string nome = "Artur " + idade;
+            string concatenar ="Ola " + nome + ".";
+
+            string interpolar =$"A idade do {nome} é {idade}." ;
+            string interpolar2 =$"O {nome} quando for velho terá"+
+            $" {idade + velhice} anos." ;
+
             Console.WriteLine("Hello, World!");
             Console.WriteLine(frase1);
+            Console.WriteLine(concatenar);
+            Console.WriteLine(interpolar);
+            Console.WriteLine(interpolar2);
         }
     }
 }
