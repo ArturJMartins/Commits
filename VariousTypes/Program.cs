4e204cfd0bda279b179efb958636e53159c0ea11
Artur Martins <arturjcmartins.dev@gmail.com>
2024-02-23 13:22:24 +0000

    Variavel char com valores unicode

--- VariousTypes/Program.cs
+++ VariousTypes/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 
 namespace VariousTypes
 {
@@ -6,12 +7,20 @@ namespace VariousTypes
     {
         static void Main(string[] args)
         {
+            Console.OutputEncoding = Encoding.UTF8;
+
             short a = 1;
             int b = 2;
             long c = 3L;
             ushort d = 4;
             uint e = 5U;
             ulong f = 6UL;
+
+            char estrelaNegra = '\u2605';
+            char vHandPose = '\u270C';
+            char copyrightSymbol = '\u00A9';
+            
+
             Console.WriteLine("Hello, World!");
             Console.WriteLine(a);
             Console.WriteLine(b);
@@ -19,6 +28,10 @@ namespace VariousTypes
             Console.WriteLine(d);
             Console.WriteLine(e);
             Console.WriteLine(f);
+
+            Console.WriteLine(estrelaNegra);
+            Console.WriteLine(vHandPose);
+            Console.WriteLine(copyrightSymbol);
         }
     }
 }
